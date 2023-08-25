806198840123e6b851ac1cd78fd0931c90006eb4
Poesitor <Poesitor1@gmail.com>
2023-08-25 10:51:18 +0000

    add prototype for format printers in header file

--- main.h
+++ main.h
@@ -10,5 +10,7 @@ typedef struct
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+void print_char(va_list args, int *count);
+void print_string(va_list args, int *count);
 
 #endif /* MAIN_H */
